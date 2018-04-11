66bb1b85cc71a9d7d76c84cca7319dab020fea72
Scott_Fitz <scott.fitzmaurice@gmail.com>
2018-04-10 20:47:43 -0500

    Added 1 second pause for each bin in list

--- binlist_check.py
+++ binlist_check.py
@@ -10,15 +10,17 @@ url = 'https://lookup.binlist.net/'
 headers = {'Accept-version': '3'}
 #bins = open('/kalimac/Documents/python_scripts/check_binlist/bin_list_num.txt', 'rb')
 #b = int(bins.readline()
-#with open('/kalimac/Documents/python_scripts/check_binlist/bin_list_num.txt') as b:
-#    for line in b:
-f = open('/kalimac/Documents/python_scripts/check_binlist/bin_list_num.txt', 'rb+')
-b = f.readline()
-b = b.strip('\n')
-print(b)
-http = requests.get(url+b,headers=headers)
-f.close()
-content = http.content
-json_data = json.loads(content)
-for i in json_data.keys():
-    print (i+" : "+str(json_data[i]))
+with open('/kalimac/Documents/python_scripts/check_binlist/bin_list_num.txt') as line:
+    for b in line:
+#f = open('/kalimac/Documents/python_scripts/check_binlist/bin_list_num.txt', 'rb+')
+        #b = f.readline()
+        time.sleep(1)
+        b = b.strip('\n')
+        print('-'*10)
+        print(b)
+        print('-'*10)
+        http = requests.get(url+b,headers=headers)
+        content = http.content
+        json_data = json.loads(content)
+        for i in json_data.keys():
+            print (i+" : "+str(json_data[i]))
