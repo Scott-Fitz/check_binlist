087617a9b7e1d01fd45966b44e3d932bbc69b01f
Scott_Fitz <scott.fitzmaurice@gmail.com>
2018-04-10 20:26:28 -0500

    Reads from a File

--- binlist_check.py
+++ binlist_check.py
@@ -6,10 +6,18 @@ import json
 import time
 
 url = 'https://lookup.binlist.net/'
-b = raw_input("Insert a bin: ")
+#b = raw_input("Insert a bin: ")
 headers = {'Accept-version': '3'}
-
+#bins = open('/kalimac/Documents/python_scripts/check_binlist/bin_list_num.txt', 'rb')
+#b = int(bins.readline()
+#with open('/kalimac/Documents/python_scripts/check_binlist/bin_list_num.txt') as b:
+#    for line in b:
+f = open('/kalimac/Documents/python_scripts/check_binlist/bin_list_num.txt', 'rb+')
+b = f.readline()
+b = b.strip('\n')
+print(b)
 http = requests.get(url+b,headers=headers)
+f.close()
 content = http.content
 json_data = json.loads(content)
 for i in json_data.keys():
